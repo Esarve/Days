d4c9d1556e7715f4d5071f4ed4308451ead01860
Esarve <esarve.srv@gmail.com>
2018-12-07 15:40:46 +0600

    Fragments added and activity for adding texts

--- app/src/main/java/bd/diu/sourav/days/MainActivity.java
+++ app/src/main/java/bd/diu/sourav/days/MainActivity.java
@@ -2,8 +2,12 @@ package bd.diu.sourav.days;
 
 import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +21,37 @@ public class MainActivity extends AppCompatActivity {
         if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setNavigationBarColor(getResources().getColor(R.color.NavColor));
         }
+
+        BottomNavigationView bottomNav = findViewById(R.id.btm_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navlistner);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_cont, new DefaultFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectFrag = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectFrag = new DefaultFragment();
+                            break;
+                        case R.id.nav_stats:
+                            selectFrag = new StatsFragment();
+                            break;
+                        case R.id.nav_about:
+                            selectFrag = new AboutFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_cont,selectFrag).commit();
+                    return true;
+                }
+            };
+
+
 
     public void openEditor(View view){
         intent = new Intent(this, TextInput.class);

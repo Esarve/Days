65c9264334528f0ea1ab934e093d96f0913d0077
Esarve <esarve.srv@gmail.com>
2018-12-07 13:52:04 +0600

    New activity (Edit text) on FAB click added

--- app/src/main/java/bd/diu/sourav/days/TextInput.java
+++ app/src/main/java/bd/diu/sourav/days/TextInput.java
@@ -1,26 +1,17 @@
 package bd.diu.sourav.days;
 
-import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
-    Intent intent;
+public class TextInput extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_text_input);
         if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setNavigationBarColor(getResources().getColor(R.color.NavColor));
         }
-    }
-
-    public void openEditor(View view){
-        intent = new Intent(this, TextInput.class);
-        this.startActivity(intent);
     }
 }
